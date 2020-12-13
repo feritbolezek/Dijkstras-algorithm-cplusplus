d5364ecbd5a89780c4031d985e67c6c215578272
feritbolezek <developer@feritbolezek.com>
2020-12-13 21:13:31 +0100

    Set up the code files

---
+++ Dijkstra/Edge.cc
@@ -0,0 +1,10 @@
+
+#include "Edge.h"
+
+Node* Edge::getDestination() {
+    // TODO: Implement me.
+}
+
+int Edge::getLength() const {
+    // TODO: Implement me.
+}
---
+++ Dijkstra/Node.cc
@@ -0,0 +1,22 @@
+
+#include "Node.h"
+
+string Node::getName() const {
+    // TODO: Implement me.
+}
+
+void Node::setValue(int v) {
+    // TODO: Implement me.
+}
+
+int Node::getValue() const {
+    // TODO: Implement me.
+}
+
+void Node::addEdge(Node* destination, int length) {
+    // TODO: Implement me.
+}
+
+const vector<Edge>& Node::getEdges() const {
+    // TODO: Implement me.
+}
